3484873a695679ce75cabee59396f4d45c7b836c
Andre Leith <andre.leith@gmail.com>
2014-03-07 17:48:06 -0600

    Work
    work

---
+++ index.php
@@ -0,0 +1,62 @@
+<!DOCTYPE HTML>
+<html>
+	<head>
+		<meta charset="UTF-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<title>Backfat Variety Show</title>
+		<!-- Google Fonts -->
+		<link href='http://fonts.googleapis.com/css?family=Lato' rel='stylesheet' type='text/css'>
+		<link href='http://fonts.googleapis.com/css?family=Podkova' rel='stylesheet' type='text/css'>
+		<!-- Stylesheet -->
+		<link rel="stylesheet" href="styles/style.css" type="text/css" media="screen">
+		<!-- Modernizr -->
+		<script src="js/modernizr.js"></script>
+		<!-- Use Respond.js for media queries in IE etc ? -->
+		<!-- Use JQuery ? -->
+	</head>
+	<body>
+		<div id="site">
+			<header>
+				<img src="img/logo-header.png" id="logo" class="obj_center">
+			</header>	
+			<nav class="layout-nav">
+				<hr class="line_rounded">
+				<ul>
+					<li>ABOUT</li>
+					<li>BIOS</li>
+					<li>PICS</li>
+					<li>PAST PERFORMERS</li>
+					<div style="clear: both;"></div>
+				</ul>
+				<hr class="line_rounded">
+			</nav>				
+			<div id="page-content">	
+				<section class="clearfix">
+					<header>
+						<h1>Bios</h1>
+					</header>
+					<article class="layout-bios">
+						<figure class="bio-photo obj_float-left">
+							<img src="img/blank-bio-photo.png">
+						</figure>
+						<header>
+							<h2>EMILY WINTER</h2>
+						</header>
+							<p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque</p>
+					</article>
+				</section>
+			</div>
+		</div>
+		<br>
+		<br>
+		<br>
+		<br>
+		<br>
+		<br>
+		<br>
+		<br>
+		<br>
+	</body>
+
+
+</html>
